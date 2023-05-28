606497dda4e56495501304cbaabfd918f973cd8d
Eric Bailey <idemrisc@gmail.com>
2023-05-28 13:21:44 -0400

    basic setup working

--- R/lesson_4.R
+++ R/lesson_4.R
@@ -1,4 +1,8 @@
 #'@export
 lesson_4 <- function() {
-  learnr::run_tutorial("functions_loops_conditionals_4", "TrainingRIntro")
+  learnr::run_tutorial(
+    "functions_loops_conditionals_4",
+    "TrainingRIntro",
+    shiny_args=c(launch.browser=rstudioapi::viewer)
+  )
 }
