165b8f86a16490b6fcfea8f71fd0fb332da609e9
Eric Bailey <idemrisc@gmail.com>
2023-05-28 14:16:58 -0400

    added startup message

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+"    _               _____   _____ ____  \n",
+"   | |        /\\   |  __ \\ / ____/ __ \\ \n",
+"   | |       /  \\  | |  | | |   | |  | |\n",
+"   | |      / /\\ \\ | |  | | |   | |  | |\n",
+"   | |____ / ____ \\| |__| | |___| |__| |\n",
+"   |______/_/    \\_\\_____/ \\_____\\____/ \n",
+"  Lake Michigan Air Director's Consortium\n\n",
+"Introduction to R for Air Quality Data Analysis\n",
+"_______________________________________________\n\n",
+"Please have a look at the vignettes for an \n",
+"overview of the lessions in this package. Or \n",
+"you can access the interactive lessons from the \n",
+"`Addins` dropdown at the top of the screen. \n\n",
+"If you have any trouble please use the package \n",
+"help files or contact the package authors."
+)
+}
+
